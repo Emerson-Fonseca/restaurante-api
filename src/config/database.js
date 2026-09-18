527502ed369ac717a3be494519eae9d3c9018901
ALUNO SESI PETROLINA <emerson08edson@gmail.com>
2026-09-18 17:19:05 -0300

    database

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,15 @@ const db = mysql.createPool({
     database: process.env.DB_NAME,
     waitForConnections:true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+
+    ssl: {
+        rejectUnauthorized: false
+    },
+
+    waitForConnections:true,
+    connectionLimit:10,
+    queueLimit:0
 });
 
 module.exports = db;
